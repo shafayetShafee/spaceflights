c6fbdf3f75e0134ac0000fc74ec1ee01d3c8fc20
shafayetShafee <sshafee@isrt.ac.bd>
2024-07-11 01:53:18 +0600

    chore: added node in data_preproc pipeline

--- space-flights/src/space_flights/pipelines/data_processing/pipeline.py
+++ space-flights/src/space_flights/pipelines/data_processing/pipeline.py
@@ -3,8 +3,22 @@ This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.19.6
 """
 
-from kedro.pipeline import Pipeline, pipeline
+from kedro.pipeline import Pipeline, pipeline, node
+from .nodes import preprocess_companies, preprocess_shuttles
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            func=preprocess_companies,
+            inputs="companies",
+            outputs="preprocessed_companies",
+            name="preprocess_companies_node"
+        ),
+        node(
+            func=preprocess_shuttles,
+            inputs="shuttles",
+            outputs="preprocessed_shuttles",
+            name="preprocess_shuttles_node"
+        ),
+    ])
